ff87946521207c12a5988b3c8d7d0f4bd7af3750
MS\hcardoso <hernan.f.cardoso@gmail.com>
2023-07-19 21:26:09 -0300

    feat. Add Relations many to many, and relations polimorficas

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,23 +7,40 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use App\Models\User;
 use App\Models\Category;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use App\Models\Tag;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 class Post extends Model
 {
     use HasFactory;
 
 
-    //Relations 1:N (inversa) a post belong to a user.
+    //Relations 1:N (inverse) a post belong to a user.
 
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
-    //Relations 1:N (inversa) a post belong to a category.
+    //Relations 1:N (inverse) a post belong to a category.
 
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
+    }
+
+    //Relations N:N, many posts belong to many tags
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
+    //Relations 1:1 Polimorfica
+
+    public function image(): MorphOne
+    {
+        return $this->morphOne(Image::class, 'imageable');
     }
 }
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tag extends Model
 {
     use HasFactory;
+
+    //Relations N:N, many tags belong to many posts
+
+    public function posts(): BelongsToMany
+    {
+        return $this->belongsToMany(Post::class);
+    }
 }
